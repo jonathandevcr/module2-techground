b0655f378ec64c4e6223440d5796eb55913388d1
Jonathan Gomez <jonathangomez3189@gmail.com>
2020-08-01 20:19:49 -0600

    no message

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -3,11 +3,8 @@
 * that is shared across all page objects
 */
 module.exports = class Page {
-    /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
-    */
+    
     open (path) {
-        return browser.url(`https://demo.realworld.io/`)
+        return browser.url(`https://demo.realworld.io/${path}`)
     }
 }
